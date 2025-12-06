cf2d4c1c3399eb2c162edb7d2947854c36e678aa
Sara <ssuarezlo@unal.edu.co>
2025-12-06 11:21:49 -0500

    Ejecucion de modelos

---
+++ make_predictions.py
@@ -0,0 +1,29 @@
+PREDICTIONS  ####
+
+"""Prediccion script for the MLflow model.
+
+This script loads a model from MLflow and makes predictions on a dataset.
+
+$ python3 make_predictions.py
+
+
+"""
+
+import mlflow
+import pandas as pd
+
+FILE_PATH = "data/winequality-red.csv"
+
+
+df = pd.read_csv(FILE_PATH)
+y = df["quality"]
+x = df.drop(columns=["quality"])
+
+## Debe verificarse el run_id del modelo que se quiere cargar
+## Se puede obtener el run_id desde la interfaz de MLflow
+
+logged_model = "runs:/694b8603ee6a40db86f113b353fd2581/model"
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+y = loaded_model.predict(x)
+
+print(y)
